2ff78afde940039ba9d48e797f4ee1d5876d7936
GarethLapras <garethlapras@gmail.com>
2026-09-16 17:00:54 +0200

    Update works-data.js

--- js/works-data.js
+++ js/works-data.js
@@ -12,8 +12,11 @@ window.SK_WORKS = [
     role: 'Illustration, lettering, apparel design',
     desc: 'Commissioned t-shirt and merch design built around Isaiah 61:3, pairing botanical line art with a wildflower field illustration.',
     images: [
+      'assets/works/branding-merch-product/BfA_Single.jpg',
+      'assets/works/branding-merch-product/BfA_Duo.jpg',
+      'assets/works/branding-merch-product/BfA_Graphics.jpg',
+      'assets/works/branding-merch-product/BfA_Outline.jpg',
       'assets/works/branding-merch-product/BfA_Concept.jpg',
-      'assets/works/branding-merch-product/BfA_Outline.jpg'
     ],
     vertical: false,
     link: '',
